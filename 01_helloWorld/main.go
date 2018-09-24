0d2a8dfe7c3e8cf1fe1f2ab55e3293fc5cb9fded
methowweb <methowweb@gmail.com>
2018-09-24 00:33:05 -0700

    combined lessons in one file. no save between.

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -2,11 +2,20 @@ package main
 
 import "fmt"
 
+var x = 8
+
 func main() {
+	y := 5
+	z := 4
 	fmt.Println("hello World")
-	for i := 0; i < 200; i++ {
-		fmt.Printf("%d \n", i)
+	for i := 0; i < 4; i++ {
+		// fmt.Printf("%d \n", i)
 		// test line
 		//test line 2 plus more and backcheck
+		//fmt.Println(y)
+		fmt.Printf("\t %d next   a     %d \n", z, y)
+		fmt.Printf("\t %d next  %d \n", z, y)
+		//fmt.Printf("\t %v \t %v \n", z, y)
 	}
+	//fmt.Println(x)
 }
