a707ed7f9665899064c5e86b1e55dd4e581376de
methowweb <methowweb@gmail.com>
2018-09-24 18:52:04 -0700

    commit from terminal

--- 002 current lesson/main.go
+++ 002 current lesson/main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 const p string = "death and taxes"
+const ( _ = iota
+		kb = 1 <<(iota *10)
+		mb = 1 << (iota *10)
+
+)
 
 func main() {
 
@@ -10,5 +15,14 @@ func main() {
 
 	fmt.Println("p - ", p)
 	fmt.Println("q - ", q)
+
+	fmt.Println("binary\t\tdecimal")
+	fmt.Printf("%b\t", kb)
+	fmt.Printf("%d\n",kb)
+	fmt.Printf("%b\t",mb)
+	fmt.Printf("%d\n",mb)
+
+
+
 
 }
