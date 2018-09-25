ae04c410216593ecf1d899033e434d1d129bc34b
methowweb <methowweb@gmail.com>
2018-09-25 14:50:12 -0700

    not showing format

--- 003 morepractice/main.go
+++ 003 morepractice/main.go
@@ -1,1 +1,21 @@
-package _03_morepractice
+package main
+
+import "fmt"
+
+func main() {
+
+	i := 0
+	for {
+		i++
+		if i%1000 != 0 {
+			continue
+		}
+		fmt.Println(i)
+		fmt.Println("remainder", i%1000)
+		if i >= 50000 {
+			break
+		}
+
+	}
+
+}
