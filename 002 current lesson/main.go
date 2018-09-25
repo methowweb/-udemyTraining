8332a3024f6a044ea9d9301b372586954c88a3a6
methowweb <methowweb@gmail.com>
2018-09-24 19:48:45 -0700

    Practicing inputs and keeping terminal open

--- 002 current lesson/main.go
+++ 002 current lesson/main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 const p string = "death and taxes"
-const ( _ = iota
-		kb = 1 <<(iota *10)
-		mb = 1 << (iota *10)
-
+const (
+	_  = iota
+	kb = 1 << (iota * 10)
+	mb = 1 << (iota * 10)
 )
 
 func main() {
@@ -16,13 +16,22 @@ func main() {
 	fmt.Println("p - ", p)
 	fmt.Println("q - ", q)
 
-	fmt.Println("binary\t\tdecimal")
+	/*fmt.Println("binary\t\tdecimal")
 	fmt.Printf("%b\t", kb)
-	fmt.Printf("%d\n",kb)
-	fmt.Printf("%b\t",mb)
-	fmt.Printf("%d\n",mb)
+	fmt.Printf("%d\n", kb)
+	fmt.Printf("%b\t", mb)
+	fmt.Printf("%d\n", mb)*/
 
+	var meters string
+	var userinput string
+	fmt.Print("Enter something ")
+	fmt.Scan(&meters)
 
+	fmt.Println("You typed ", meters)
 
+	//fmt.Println(meters)
+	fmt.Print("Type something and Press enter to exit ")
+	fmt.Scan(&userinput)
+	fmt.Print("Finished ")
 
 }
