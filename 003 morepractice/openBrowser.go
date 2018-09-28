bd2bbbd0c10d9227f0175eb547435005f4b387f7
methowweb <methowweb@gmail.com>
2018-09-27 17:25:44 -0700

    Code from web that opens the browser for output

---
+++ 003 morepractice/openBrowser.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+	"runtime"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	go open("http://localhost:8080/")
+	http.HandleFunc("/", handler)
+	panic(http.ListenAndServe(":8080", nil))
+}
+
+func open(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
+}
--- 003 morepractice/second.go
+++ 003 morepractice/second.go
@@ -8,6 +8,9 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 func someFunc(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("Hello universe"))
+
+	apple := "hello apple"
+	w.Write([]byte("Hello universe /n"))
+	w.Write([]byte(apple))
 
 }
