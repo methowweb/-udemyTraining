81d7e1c916f584700623c7a8bb6dda076119e34f
methowweb <methowweb@gmail.com>
2018-09-29 16:02:50 -0700

    new commit

--- 004_functions/functions.go
+++ 004_functions/functions.go
@@ -6,9 +6,22 @@ func main() {
 
 	fmt.Println("hello again")
 	nextLine()
+	third := thirdLine
+	third()
+	fourthLine(22)
 }
 
 func nextLine() {
 
 	fmt.Println("next line")
+}
+
+func thirdLine() {
+
+	fmt.Println("third line")
+}
+
+func fourthLine(x int) {
+
+	fmt.Println("fourth line-- ", x)
 }
