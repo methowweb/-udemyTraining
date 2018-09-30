db484552fad0c55d7c0d145dd3ae298ccf58ecb6
methowweb <methowweb@gmail.com>
2018-09-29 23:31:59 -0700

    functions update

--- 004_functions/funcfolder/func_firstLoop.go
+++ 004_functions/funcfolder/func_firstLoop.go
@@ -1,23 +1,21 @@
 package funcfolder2
 
-
-
 import "fmt"
 
 func FirstLoop() {
 	var foo int
 	var evenOrOdd string
 	fmt.Println(foo)
-	foo ++
+	foo++
 	fmt.Println(foo, "end")
-	for i :=1; i<=10; i++ {
+	for i := 1; i <= 10; i++ {
 		fmt.Println(i)
-		fmt.Printf("the number is %v\n",i)
-		if i%2==0 {
-			evenOrOdd= "Even"
-		}else {
-			evenOrOdd= "Odd"
+		fmt.Printf("the number is %v\n", i)
+		if i%2 == 0 {
+			evenOrOdd = "Even"
+		} else {
+			evenOrOdd = "Odd"
 		}
-		fmt.Printf("%v is an %v number  and the remainder is %v\n",i,evenOrOdd, i%2)
+		fmt.Printf("%d is an %v number  and the remainder is %v\n", i, evenOrOdd, i%2)
 	}
 }
